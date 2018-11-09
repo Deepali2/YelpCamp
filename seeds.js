d22452c5c467d1c500553c5d321a4fb3b9ae3052
Deepali2 <hdgarg@gmail.com>
2018-11-09 07:21:02 -0800

    adds styling to the show page

--- seeds.js
+++ seeds.js
@@ -7,17 +7,17 @@ const data =[
   {
     name: "Crystal Cove State Park",
     image: "https://img.hipcamp.com/images/c_fill,f_auto,g_auto,h_504,q_60,w_770/v1506230132/campground-photos/zgxxiekyx0bj2sk6zt6n/tent_picnic-table_lodging_rusty-hill-farm-homestead.jpg",
-    description: loremIpsum({count: 1, units: "sentences"}),
+    description: loremIpsum({count: 20, units: "sentences"}),
   },
   {
     name: "Yosemite National Park", 
     image: "https://img.hipcamp.com/images/c_fill,f_auto,g_auto,h_504,q_60,w_770/v1530031475/campground-photos/xqocbthdsyi8vd6e3wi8/enchanted-forest-cosmo-campsite.jpg",
-    description: loremIpsum({count: 1, units: "sentences"}),
+    description: loremIpsum({count: 20, units: "sentences"}),
   },
   {
     name: "The Lost Coast", 
     image: "https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144594f7c179aee5b7_340.jpg",
-    description: loremIpsum({count: 1, units: "sentences"}),
+    description: loremIpsum({count: 20, units: "sentences"}),
   }
 ];
 
