2059b5441493cddc7fd7fc37e26782b4ac8d9cb6
Deepali2 <hdgarg@gmail.com>
2018-11-24 16:07:53 -0800

    add the mLab

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const commentRoutes    = require("./routes/comments"),
       campgroundRoutes = require("./routes/campgrounds"),
       authRoutes       = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/campground_app", {useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost:27017/campground_app", {useNewUrlParser: true});
+mongoose.connect("mongodb://deepali:Mango2018deployWebpages@ds111476.mlab.com:11476/yelpcamp1");
 
 //tell express to use body-parser and other middleware
 app.use(bodyParser.urlencoded({extended: true}));
