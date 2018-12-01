b8bd2af312e3f591c50de4643a1e8fa2ae856c66
Deepali2 <hdgarg@gmail.com>
2018-11-30 18:00:13 -0800

    remove useCreateIndex: true

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const commentRoutes    = require("./routes/comments"),
 console.log(process.env.DATABASEURL);
 
 const url = process.env.DATABASEURL || "mongodb://localhost:27017/campground_app";
-mongoose.connect(url, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(url, {useNewUrlParser: true});
 
 // mongoose.connect("mongodb://localhost:27017/campground_app", {useNewUrlParser: true});
 // mongoose.connect("mongodb://deepali:Mango2018deployWebpages@ds111476.mlab.com:11476/yelpcamp1", {useNewUrlParser: true});
